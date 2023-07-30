351610f68fe46a8e9c5ef42954a1d77a51238fab
JamesMylan <james.mylan1@education.nsw.gov.au>
2023-07-30 13:34:02 +1000

    Add background image and window title

--- gui.py
+++ gui.py
@@ -8,14 +8,16 @@ windowwidth = 600
 windowheight = 600
 offset=50 #how far the grid should be from the window border
 screen = pygame.display.set_mode((windowwidth, windowheight))
+pygame.display.set_caption("Connect Boxes")
 clock = pygame.time.Clock()
 running = True
 dt = 0
+background = pygame.image.load("images/background.jpg")
 gridsize = 10
 gamegrid = np.full((gridsize,gridsize), " ")
 running = True
 font =  pygame.font.SysFont("Arial",30)
-playerwintext = font.render(("Player Wins"),True,(0,0,0))
+playerwintext = font.render(("Player Wins"),True,(255,255,255))
 aiwintext = font.render(("AI Wins"),True,(0,0,0))
 clickpos = 0,0
 def win(value):
@@ -38,6 +40,7 @@ while running:
             clickpos = pygame.mouse.get_pos()
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
+    screen.blit(background,(0,0))
     blocksize = int((windowwidth-(2*offset))/10) #Set the size of the grid block
     for x in range(offset, windowwidth-offset, blocksize):
         for y in range(offset, windowheight-offset, blocksize):
@@ -58,6 +61,7 @@ while running:
                 pygame.Surface.fill(screen,"red",rect)
                 pygame.draw.rect(screen, "black", rect, 1)
             else:
+                pygame.Surface.fill(screen,"white",rect)
                 pygame.draw.rect(screen, "black", rect, 1)
     if main.checkingrid(gamegrid,"XXXXX"):
         win("X")
